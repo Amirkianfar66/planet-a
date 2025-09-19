624ede0b485e6ea6208cc9cb2fe951c187de1b29
Amirkianfar66 <amir.kianfar@limenet.tech>
2025-09-19 16:05:38 +0200

    f

---
+++ src/components/characters3d/Pets3D.jsx
@@ -0,0 +1,24 @@
+﻿import React from "react";
+import useItemsSync from "../systems/useItemsSync.js";
+import RobotDog from "./RobotDog.jsx";
+
+export default function Pets3D() {
+    const { items } = useItemsSync();
+    const pets = (items || []).filter(i => i.type === "pet");
+
+    if (!pets.length) return null;
+
+    return (
+        <group>
+            {pets.map(pet => (
+                <group
+                    key={pet.id}
+                    position={[Number(pet.x || 0), Number(pet.y ?? 0), Number(pet.z || 0)]}
+                    rotation={[0, Number(pet.yaw || 0), 0]}
+                >
+                    <RobotDog />
+                </group>
+            ))}
+        </group>
+    );
+}
